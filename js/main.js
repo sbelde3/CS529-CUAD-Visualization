320f34ef4fba825a710d389c8bee6eeb3080d4bc
sunilbelde <sunil.belde102@gmail.com>
2022-11-09 13:20:04 -0600

    created Contracts by Country Visualization

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* Get or create the application global variable */
+var App = App || {};
+
+var contractsByCountryCsv = "../data/Contracts_By_Geo_Date_Missing_Counts.csv";
+/* IIFE to initialize the main entry of the application*/
+(function() {
+
+    // setup the pointer to the scope 'this' variable
+    var self = this;
+
+    /* Entry point of the application */
+    App.start = function()
+    {
+
+        const data = new plot_By_Country(contractsByCountryCsv);
+
+    };
+
+}) ();
